b4d5bd1ec9e4bdf08a6690d65749bcb93c491ef7
Arnold Scott <arnvscott@gmail.com>
2019-10-14 07:32:25 +0000

    updated url pattern for the shops index view to a blank

--- nearbyshops/nearbyshops/urls.py
+++ nearbyshops/nearbyshops/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('shops/', include('shops.urls')),
+    path('', include('shops.urls')),
     path('admin/', admin.site.urls),
 ]
