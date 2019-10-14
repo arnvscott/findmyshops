ff8ff8f2c0a279782de5c852fae014251a477944
Arnold Scott <arnvscott@gmail.com>
2019-10-14 10:13:18 +0000

    updated shops app index.html to get shops from the database

--- nearbyshops/shops/views.py
+++ nearbyshops/shops/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
+from shops.models import Shop
 # Create your views here.
 
 def index(request):
         context = {}
+        shops_list = Shop.objects.all()
+        context = {'shops' : shops_list}
         return render(request, 'shops/index.html', context)
