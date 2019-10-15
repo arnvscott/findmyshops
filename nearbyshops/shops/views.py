75e39c9978268fc1e85eb027664701ff4f10d430
Arnold Scott <arnvscott@gmail.com>
2019-10-15 04:38:31 +0000

    Add address form to the shop app index html template

--- nearbyshops/shops/views.py
+++ nearbyshops/shops/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 
 from shops.models import Shop
+from shops.forms import AddressForm
 # Create your views here.
 
 def index(request):
         context = {}
         shops_list = Shop.objects.all()
-        context = {'shops' : shops_list}
+        addressform = AddressForm()
+        context = {'shops' : shops_list, 'form' : addressform}
         return render(request, 'shops/index.html', context)
