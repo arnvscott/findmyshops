d550d67cbe91a448ad0754e81d9af4c461c00899
Arnold Scott <arnvscott@gmail.com>
2019-10-15 10:24:45 +0000

    added search radius field to shops listing form

--- nearbyshops/shops/forms.py
+++ nearbyshops/shops/forms.py
@@ -3,3 +3,4 @@ from django import forms
 class AddressForm (forms.Form):
     latitude = forms.DecimalField(widget=forms.NumberInput(attrs={'class':'form-control', 'placeholder':'latitude eg. 123.456789', 'id':'latitude'}),label='Latitude')
     longitude = forms.DecimalField(widget=forms.NumberInput(attrs={'class':'form-control', 'placeholder':'longitude eg. 123.456789', 'id':'longitude'}),label='Longitude')
+    radius = forms.DecimalField(widget=forms.NumberInput(attrs={'class':'form-control', 'placeholder':'radius(in km) eg. 123', 'id':'radius'} ))
