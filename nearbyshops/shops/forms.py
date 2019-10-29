590255043cdf5742c99abbb23bdcebaa26fc518a
Arnold Scott <arnvscott@gmail.com>
2019-10-29 06:45:46 +0000

    Replace Bootstrap code with Vanilla HTML

--- nearbyshops/shops/forms.py
+++ nearbyshops/shops/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class AddressForm (forms.Form):
-    latitude = forms.DecimalField(widget=forms.NumberInput(attrs={'class':'form-control', 'placeholder':'latitude eg. 123.456789', 'id':'latitude'}),label='Latitude')
-    longitude = forms.DecimalField(widget=forms.NumberInput(attrs={'class':'form-control', 'placeholder':'longitude eg. 123.456789', 'id':'longitude'}),label='Longitude')
-    radius = forms.DecimalField(widget=forms.NumberInput(attrs={'class':'form-control', 'placeholder':'radius(in km) eg. 123', 'id':'radius'} ))
+    latitude = forms.DecimalField(label='Latitude')
+    longitude = forms.DecimalField(label='Longitude')
+    radius = forms.DecimalField(label='Radius')
