8bc8ac84e71ea7451a0cc08045ce82b8682ce63a
Arnold Scott <arnvscott@gmail.com>
2019-10-30 11:32:08 +0000

    added placeholder texts to input fields

--- nearbyshops/shops/forms.py
+++ nearbyshops/shops/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class AddressForm (forms.Form):
-    latitude = forms.DecimalField(label='Latitude', widget=forms.NumberInput(attrs={'class':'wide_input'}))
-    longitude = forms.DecimalField(label='Longitude', widget=forms.NumberInput(attrs={'class':'wide_input'}))
-    radius = forms.DecimalField(label='Radius', widget=forms.NumberInput(attrs={'class':'wide_input'}))
+    latitude = forms.DecimalField(label='Latitude', widget=forms.NumberInput(attrs={'class':'wide_input', 'placeholder':'latitude eg. 123.456789'}))
+    longitude = forms.DecimalField(label='Longitude', widget=forms.NumberInput(attrs={'class':'wide_input', 'placeholder':'longitude eg. 123.456789'}))
+    radius = forms.DecimalField(label='Radius', widget=forms.NumberInput(attrs={'class':'wide_input','placeholder':'range eg. 3'}))
