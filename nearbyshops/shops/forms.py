9d3920f18a406d7620ddf442b09d15c7e0e0eb13
Arnold Scott <arnvscott@gmail.com>
2019-10-30 08:47:15 +0000

    Added style.css and styling to index.html

--- nearbyshops/shops/forms.py
+++ nearbyshops/shops/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class AddressForm (forms.Form):
-    latitude = forms.DecimalField(label='Latitude')
-    longitude = forms.DecimalField(label='Longitude')
-    radius = forms.DecimalField(label='Radius')
+    latitude = forms.DecimalField(label='Latitude', widget=forms.NumberInput(attrs={'class':'wide_input'}))
+    longitude = forms.DecimalField(label='Longitude', widget=forms.NumberInput(attrs={'class':'wide_input'}))
+    radius = forms.DecimalField(label='Radius', widget=forms.NumberInput(attrs={'class':'wide_input'}))
