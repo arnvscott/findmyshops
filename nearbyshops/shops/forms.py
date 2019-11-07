91f5861e569cda25cc28e879dfe778ee0ac70d84
Arnold Scott <arnvscott@gmail.com>
2019-11-07 11:06:22 +0000

    Restyled footer menu bar and removed google maps icons from map

--- nearbyshops/shops/forms.py
+++ nearbyshops/shops/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class AddressForm (forms.Form):
-    latitude = forms.DecimalField(label='Latitude', widget=forms.NumberInput(attrs={'class':'wide_input', 'placeholder':'latitude eg. 123.456789'}))
-    longitude = forms.DecimalField(label='Longitude', widget=forms.NumberInput(attrs={'class':'wide_input', 'placeholder':'longitude eg. 123.456789'}))
-    radius = forms.DecimalField(label='Radius', widget=forms.NumberInput(attrs={'class':'wide_input','placeholder':'range eg. 3'}))
+    latitude = forms.DecimalField(label='Latitude', widget=forms.NumberInput(attrs={'class':'full-width','placeholder':'latitude eg. 123.456789'}))
+    longitude = forms.DecimalField(label='Longitude', widget=forms.NumberInput(attrs={'class':'full-width','placeholder':'longitude eg. 123.456789'}))
+    radius = forms.DecimalField(label='Radius', widget=forms.NumberInput(attrs={'class':'full-width','placeholder':'range eg. 3'}))
