518ab9fcab7d79e6920f1961632008e7e6c53ed1
Arnold Scott <arnvscott@gmail.com>
2019-12-03 01:06:37 +0000

    Removed all 'changeorigin' functionality

--- nearbyshops/shops/forms.py
+++ nearbyshops/shops/forms.py
@@ -1,9 +1,5 @@
 from django import forms
 
-class ChangeOriginForm (forms.Form):
-    latitude = forms.DecimalField(label='Latitude', widget=forms.NumberInput(attrs={'class':'full-width','placeholder':'latitude eg. 123.456789'}))
-    longitude = forms.DecimalField(label='Longitude', widget=forms.NumberInput(attrs={'class':'full-width','placeholder':'longitude eg. 123.456789'}))
-    radius = forms.DecimalField(label='Radius',  widget=forms.NumberInput(attrs={'class':'full-width','placeholder':'range eg. 3'}))
 
 class CurrentLocationForm (forms.Form):
     latitude = forms.DecimalField(label='Latitude', widget=forms.NumberInput(attrs={'class':'full-width', 'id':'cur_lat'}))
--- nearbyshops/shops/views.py
+++ nearbyshops/shops/views.py
@@ -2,37 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
 from shops.models import Shop
-from shops.forms import ChangeOriginForm, CurrentLocationForm
-
+from shops.forms import CurrentLocationForm
 
 from django.contrib.gis.geos import GEOSGeometry
 from django.contrib.gis.db.models.functions import Distance
 from django.contrib.gis.measure import D # ``D`` is a shortcut for ``Distance``
-
+ 
 # Create your views here.
 
 def index(request):
         context = {}
         shops_list = []
-        originform = ChangeOriginForm()
         locationform = CurrentLocationForm()
-        context = {'shops' : shops_list, 'origin_form' : originform, 'location_form': locationform}
-        return render(request, 'shops/index.html', context)
-
-def changeorigin(request):
-        context = {}
-        shops_list = []
-        form = ChangeOriginForm(request.GET)
-        if form.is_valid():
-                latitude = form.cleaned_data['latitude']
-                longitude = form.cleaned_data['longitude']
-                radius = form.cleaned_data['radius']
-                pnt = GEOSGeometry(f'POINT({latitude} {longitude} )', srid=4326)
-                if not radius:
-                        shops_list = Shop.objects.annotate(distance=Distance('location',pnt)).order_by('distance')
-                else:
-                        shops_list = Shop.objects.filter(location__distance_lte=(pnt, D(km=radius))).annotate(distance=Distance('location',pnt)).order_by('distance')
-                context = {'shops' : shops_list, 'form' : form}
+        context = {'shops' : shops_list, 'location_form': locationform}
         return render(request, 'shops/index.html', context)
 
 def getlocations(request):
@@ -40,7 +22,6 @@ def getlocations(request):
         locations = []
         target_list = []
         radius = 10
-        originform = ChangeOriginForm()
         locationform = CurrentLocationForm()
         form = CurrentLocationForm(request.GET)
         if form.is_valid():
@@ -51,7 +32,7 @@ def getlocations(request):
                 target_list = [{'name':site.name,'lat':site.location.x, 'lng':site.location.y} for site in locations]
                 print (target_list)
         
-        context = {'locations':target_list, 'origin_form':originform, 'location_form':locationform}
+        context = {'locations':target_list, 'location_form':locationform}
         return render(request, 'shops/locations.html', context)
 
 
